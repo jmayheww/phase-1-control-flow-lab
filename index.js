f79b9536baf8e9c1bf32bc3df85409fa7f8fb636
Joshua <joshsmayhew@gmail.com>
2022-06-07 16:59:59 +0700

    commit tree 6d107cc18f46cfee23e783e6ead3329640bcc39f

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    console.log("This one is on me!")
+    return "This one is on me!";
+  } else if (someValue > 400 && someValue < 2000) {
+    console.log("That will be twenty bucks.")
+    return "That will be twenty bucks."
+  } else if (someValue > 2000 && someValue <= 2500) {
+    console.log("I will gladly take your thirty bucks.")
+    return "I will gladly take your thirty bucks."
+  } else return "No can do."
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+};
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+scuberGreetingForFeet(199);
+scuberGreetingForFeet(1500);
+scuberGreetingForFeet(2001);
+scuberGreetingForFeet(2501);
+
+function ternaryCheckCity(city) {
+
+
+  const isNYC = city === "NYC" ? "Ok, sounds good." : "No go."
+  return isNYC;
+};
+
+ternaryCheckCity("NYC");
+ternaryCheckCity("Pittsburgh");
+
+
+function switchOnCharmFromTip(tip) {
+
+  let reply;
+
+
+    switch (tip) {
+      case 'generous':
+      return reply = "Thank you so much."
+      case 'not as generous':
+        return reply = "Thank you."
+      default:
+        return reply = "Bye."
+    }
+};
+
+switchOnCharmFromTip("generous");
+switchOnCharmFromTip("not as generous");
+switchOnCharmFromTip("thanks for everything");
